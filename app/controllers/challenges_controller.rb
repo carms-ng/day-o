076617a37aeba6f27841344c1ef5223184966395
Nour Souid <noursouidd@gmail.com>
2020-03-03 15:24:26 -0500

    categories

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,7 +3,6 @@ class ChallengesController < ApplicationController
   def index
     category = Category.find(params[:category_id])
     @challenge = Challenge.joins(:challenge_categories).where(challenge_categories: { category_id: category.id })
-    # @challenge = Challenge.joins(:challenge_categories).where(challenge_categories: { category_id: 1 })
   end
 
   def all
