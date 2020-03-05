0c86b488eb3a1eab3091a651f36137efc24456d7
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-03-05 18:07:26 -0500

    Added bar chart to profile and started to add black badges

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,55 @@
 import "bootstrap";
+
+import Chart from 'chart.js';
+
+const ctx = document.getElementById('weekChart');
+
+const data = JSON.parse(ctx.dataset.impact)
+
+console.log(data)
+
+const xAxis = Object.keys(data);
+const yAxis = Object.values(data);
+console.log(xAxis);
+console.log(yAxis);
+
+const weekChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: xAxis,
+        datasets: [{
+            label: 'Weekly Impact',
+            data: yAxis,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
