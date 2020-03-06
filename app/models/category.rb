ba95d947783658adbfd1748eb9be6e719e2b4e2c
Nour Souid <noursouidd@gmail.com>
2020-03-05 21:02:14 -0500

    typo

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :action_categories, dependent: :destroy
   has_many :actions, through: :action_categories
-  has_many :user_ategories, dependent: :destroy
+  has_many :user_categories, dependent: :destroy
   has_many :users, through: :user_ategories
 
 end
