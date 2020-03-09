9bc905923c894d8d63938ff33248d88717383373
Laurence Berube Cote <lberubecote@hotmail.com>
2020-03-09 16:48:34 -0400

    buddy_challenge update

--- app/controllers/buddy_challenges_controller.rb
+++ app/controllers/buddy_challenges_controller.rb
@@ -5,6 +5,8 @@ class BuddyChallengesController < ApplicationController
   end
 
   def create
-    raise
+    @buddy_challenge = BuddyChallenge.new(receiver_id: params[:buddy_challenge][:receiver_id], sender_id: current_user.id, challenge_id: params[:challenge_id], status: false)
+    @buddy_challenge.save
+    redirect_to buddy_challenges_path
   end
 end
