c133aad6d04d2dfd8683cb1199d3ad14a3b9a438
Laurence Berube Cote <lberubecote@hotmail.com>
2020-03-10 17:44:30 -0400

    homepage

---
+++ app/javascript/components/homepage.js
@@ -0,0 +1,8 @@
+console.log("hello from homepage")
+
+const typed = new Typed('.element', {
+  strings: ["First sentence.", "Second sentence."],
+  typeSpeed: 30
+});
+
+typed();
---
+++ app/javascript/packs/homepage.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+
+
+const typed = new Typed('#typed-tagline', {
+  strings: ["A Climate Battle We Can Win. Together."],
+  typeSpeed: 140,
+  loop: true
+});
+
